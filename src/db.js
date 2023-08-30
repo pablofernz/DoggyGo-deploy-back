ec051d53357ea5b22bd6640774da847a085d90bb
pabloferz <pablodanyfer@gmail.com>
2023-08-30 19:18:19 -0300

    break intencional

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { DB_DEPLOY } = process.env;
 
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(DB_DEPLO, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectOptions: {
